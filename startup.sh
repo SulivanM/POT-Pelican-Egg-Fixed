9e47ecd7e7e5588a9ad8d0b59906bef8a7460d9a
SulivanM <139988886+SulivanM@users.noreply.github.com>
2024-04-25 00:20:12 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,6 +3,12 @@ echo "Path Of Titans Egg Star-X"
 echo "(c) Copyright 2024 - SwitchCompagnie"
 echo "Starting Server"
 
+echo "-------------------------------------------------------------------------------------------------"
+echo "Checking Updates"
+echo "-------------------------------------------------------------------------------------------------"
 ./AlderonGamesCmd-Linux --game path-of-titans --server true --beta-branch $BETA_BRANCH --auth-token $AG_AUTH_TOKEN --install-dir ./POT/
 
-./POT/PathOfTitansServer.sh ${SERVER_MAP}?listen?MaxPlayers=${SERVER_MAX_PLAYERS} -nullRHI -ServerName=${SERVER_NAME} -ServerGUID=${SERVER_GUID} -BranchKey=${BETA_BRANCH} -Database=${SERVER_DB} -log
+echo "-------------------------------------------------------------------------------------------------"
+echo "Starting POT Server"
+echo "-------------------------------------------------------------------------------------------------"
+./POT/PathOfTitansServer.sh -log -Port=$SERVER_PORT -BranchKey=$BETA_BRANCH -AuthToken=$AG_AUTH_TOKEN -ServerGUID=$SERVER_GUID -Database=Loca
