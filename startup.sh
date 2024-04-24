9372c98e4f178e8c3c6cec2a463baca06c58c18c
SulivanM <139988886+SulivanM@users.noreply.github.com>
2024-04-24 23:45:04 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -5,4 +5,4 @@ echo "Starting Server"
 
 ./AlderonGamesCmd-Linux --game path-of-titans --server true --beta-branch $BETA_BRANCH --auth-token $AG_AUTH_TOKEN --install-dir ./PathOfTitans/
 
-./server/PathOfTitansServer.sh ${SERVER_MAP}?listen?MaxPlayers=${SERVER_MAX_PLAYERS} -nullRHI -ServerName=${SERVER_NAME} -ServerGUID=${SERVER_GUID} -BranchKey=${BETA_BRANCH} -Database=${SERVER_DB} -log
+./PathOfTitans/PathOfTitansServer.sh ${SERVER_MAP}?listen?MaxPlayers=${SERVER_MAX_PLAYERS} -nullRHI -ServerName=${SERVER_NAME} -ServerGUID=${SERVER_GUID} -BranchKey=${BETA_BRANCH} -Database=${SERVER_DB} -log
