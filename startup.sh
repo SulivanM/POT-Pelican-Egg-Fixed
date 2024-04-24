c317218d86ec90ad1047f4430c4e4a165b3d6f3d
SulivanM <139988886+SulivanM@users.noreply.github.com>
2024-04-24 22:36:19 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,7 +3,9 @@ echo "Path Of Titans Egg Star-X"
 echo "(c) Copyright 2024 - SwitchCompagnie"
 echo "Starting Server"
 
-server_executable="/home/container/PathOfTitans/Binaries/Linux/PathOfTitansServer-Linux-Shipping"
+UE_TRUE_SCRIPT_NAME=$(echo \"$0\" | xargs readlink -f)
+UE_PROJECT_ROOT=$(dirname "$UE_TRUE_SCRIPT_NAME")
+server_executable="UE_PROJECT_ROOT/PathOfTitans/Binaries/Linux/PathOfTitansServer-Linux-Shipping"
 
 # Vérification de l'existence du répertoire du serveur et du fichier exécutable
 if [ ! -d "$(dirname "$server_executable")" ] || [ ! -f "$server_executable" ]; then
