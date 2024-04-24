81ea15bdf863bf7e498ba1a83c8b314bee07e2bb
SulivanM <139988886+SulivanM@users.noreply.github.com>
2024-04-24 23:15:50 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,22 +3,6 @@ echo "Path Of Titans Egg Star-X"
 echo "(c) Copyright 2024 - SwitchCompagnie"
 echo "Starting Server"
 
-server_executable="mtn/server/PathOfTitans/Binaries/Linux/PathOfTitansServer-Linux-Shipping"
+./AlderonGamesCmd-Linux --game path-of-titans --server true --beta-branch $BETA_BRANCH --auth-token $AG_AUTH_TOKEN --install-dir ./server/
 
-# Vérification de l'existence du répertoire du serveur et du fichier exécutable
-if [ ! -d "$(dirname "$server_executable")" ] || [ ! -f "$server_executable" ]; then
-    echo "Error: Server directory or executable not found."
-    exit 1
-fi
-
-# Changement des permissions du serveur exécutable
-chmod +x "$server_executable"
-
-# Démarrage du serveur avec les paramètres spécifiés
-"$server_executable" PathOfTitans \
-    -log \
-    -Port="$SERVER_PORT" \
-    -BranchKey="$BETA_BRANCH" \
-    -AuthToken="$AG_AUTH_TOKEN" \
-    -ServerGUID="$SERVER_GUID" \
-    -Database=Local
+./server/PathOfTitansServer.sh ${SERVER_MAP}?listen?MaxPlayers=${SERVER_MAX_PLAYERS} -nullRHI -ServerName=${SERVER_NAME} -ServerGUID=${SERVER_GUID} -BranchKey=${BETA_BRANCH} -Database=${SERVER_DB} -log
