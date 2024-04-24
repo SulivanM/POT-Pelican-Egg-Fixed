edec1304dab9431ea40990eb9f1b8c5556a78086
SulivanM <139988886+SulivanM@users.noreply.github.com>
2024-04-25 00:57:30 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -10,4 +10,4 @@ echo "--------------------------------------------------------------------------
 echo "-------------------------------------------------------------------------------------------------"
 echo "Starting POT Server"
 echo "-------------------------------------------------------------------------------------------------"
-./POT/PathOfTitansServer.sh ${SERVER_MAP} -log -ServerName=$ServerName -MaxPlayers=$SERVER_MAX_PLAYERS -Port=$SERVER_PORT -BranchKey=$BETA_BRANCH -AuthToken=$AG_AUTH_TOKEN -ServerGUID=$SERVER_GUID -Database=$SERVER_DATABASE
+./POT/PathOfTitansServer.sh ${SERVER_MAP} -log -ServerName=$ServerName -MaxPlayers=$MAX_PLAYERS -Port=$SERVER_PORT -BranchKey=$BETA_BRANCH -AuthToken=$AG_AUTH_TOKEN -ServerGUID=$SERVER_GUID -Database=$SERVER_DATABASE
