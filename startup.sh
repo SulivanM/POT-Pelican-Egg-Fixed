f469e12b92e9a26f0962ee5e22508c4e9ee8b4b0
SulivanM <139988886+SulivanM@users.noreply.github.com>
2024-04-24 22:02:42 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,29 +1,24 @@
-#! /bin/bash
+#!/bin/bash
 echo "Path Of Titans Egg Star-X"
 echo "(c) Copyright 2024 - SwitchCompagnie"
 echo "Starting Server"
 
-# Vérification de l'existence du répertoire du serveur
-if [ ! -d "/mnt/server/PathOfTitans/Binaries/Linux/" ]; then
-    echo "Error: Server directory not found."
-    exit 1
-fi
+server_executable="/mnt/server/PathOfTitans/Binaries/Linux/PathOfTitansServer-Linux-Shipping"
 
-# Vérification de l'existence du fichier exécutable du serveur
-if [ ! -f "/mnt/server/PathOfTitans/Binaries/Linux/PathOfTitansServer-Linux-Shipping" ]; then
-    echo "Error: Server executable not found."
+# Vérification de l'existence du répertoire du serveur et du fichier exécutable
+if [ ! -d "$(dirname "$server_executable")" ] || [ ! -f "$server_executable" ]; then
+    echo "Error: Server directory or executable not found."
     exit 1
 fi
 
 # Changement des permissions du serveur exécutable
-chmod +x "/mnt/server/PathOfTitans/Binaries/Linux/PathOfTitansServer-Linux-Shipping"
+chmod +x "$server_executable"
 
 # Démarrage du serveur avec les paramètres spécifiés
-"/mnt/server/PathOfTitans/Binaries/Linux/PathOfTitansServer-Linux-Shipping" PathOfTitans \
+"$server_executable" PathOfTitans \
     -log \
-    -Port=$SERVER_PORT \
-    -BranchKey=$BETA_BRANCH \
-    -AuthToken=$AG_AUTH_TOKEN \
-    -ServerGUID=$SERVER_GUID \
+    -Port="$SERVER_PORT" \
+    -BranchKey="$BETA_BRANCH" \
+    -AuthToken="$AG_AUTH_TOKEN" \
+    -ServerGUID="$SERVER_GUID" \
     -Database=Local
-# Pour que la liste de serveurs fonctionne, vous devez ajouter l'option -ServerListIP=000.000.000
