706e7a28eab9a46f1e8503ca9b0ba2baea750b60
SulivanM <139988886+SulivanM@users.noreply.github.com>
2024-04-24 21:36:33 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,11 +1,27 @@
 #! /bin/bash
 echo "Path Of Titans Egg Star-X"
 echo "(c) Copyright 2024 - SwitchCompagnie"
-echo "Checking Updates"
-export DOTNET_BUNDLE_EXTRACT_BASE_DIR=$(pwd)
-./AlderonGamesCmd --game path-of-titans --server true --beta-branch $BETA_BRANCH --install-dir .
 echo "Starting Server"
-UE_TRUE_SCRIPT_NAME=$(echo \"$0\" | xargs readlink -f)
+
+# Récupération du répertoire actuel du script
+UE_TRUE_SCRIPT_NAME=$(realpath "$0")
 UE_PROJECT_ROOT=$(dirname "$UE_TRUE_SCRIPT_NAME")
+
+# Vérification de l'existence du répertoire du serveur
+if [ ! -d "$UE_PROJECT_ROOT/PathOfTitans/Binaries/Linux/" ]; then
+    echo "Error: Server directory not found."
+    exit 1
+fi
+
+# Changement des permissions du serveur exécutable
 chmod +x "$UE_PROJECT_ROOT/PathOfTitans/Binaries/Linux/PathOfTitansServer-Linux-Shipping"
-"$UE_PROJECT_ROOT/PathOfTitans/Binaries/Linux/PathOfTitansServer-Linux-Shipping" PathOfTitans -log -Port=$SERVER_PORT -BranchKey=$BETA_BRANCH -AuthToken=$AG_AUTH_TOKEN -ServerGUID=$SERVER_GUID -Database=Local
+
+# Démarrage du serveur avec les paramètres spécifiés
+"$UE_PROJECT_ROOT/PathOfTitans/Binaries/Linux/PathOfTitansServer-Linux-Shipping" PathOfTitans \
+    -log \
+    -Port=$SERVER_PORT \
+    -BranchKey=$BETA_BRANCH \
+    -AuthToken=$AG_AUTH_TOKEN \
+    -ServerGUID=$SERVER_GUID \
+    -Database=Local
+# Pour que la liste de serveurs fonctionne, vous devez ajouter l'option -ServerListIP=000.000.000
